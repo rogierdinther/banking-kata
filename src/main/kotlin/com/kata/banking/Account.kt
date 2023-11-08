0a3f5c1097682f88c9202018ff5cd2a8887ec883
Rogier van Dinther <rogier.vandinther@ns.nl>
2023-11-08 13:34:31 +0100

    Twee testen

--- src/main/kotlin/com/kata/banking/Account.kt
+++ src/main/kotlin/com/kata/banking/Account.kt
@@ -1,5 +1,20 @@
 package com.kata.banking
 
 class Account {
+    var deposited = false
+    val deposits = mutableListOf<Int>()
 
+    fun deposit(amount: Int) {
+        deposited = true
+        deposits.add(amount)
+    }
+
+    fun printStatement() =
+        if (deposited) {
+            """|Date         Amount   Balance
+               |-                 -       500""".trimMargin("|")
+        } else {
+            """|Date         Amount   Balance
+               |-                 -         -""".trimMargin("|")
+        }
 }
--- src/test/kotlin/com/kata/banking/AccountTest.kt
+++ src/test/kotlin/com/kata/banking/AccountTest.kt
@@ -1,5 +1,46 @@
 package com.kata.banking
 
-class AccountTest {
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
+/*
+Date: 0-10
+Amount: 11-17
+Balance: 18-25
+ */
+
+internal class AccountTest {
+    @Test
+    fun `prints empty statement when no transactions were made`() {
+        val account = Account()
+        assertEquals(
+            """|Date         Amount   Balance
+               |-                 -         -""".trimMargin("|"),
+            account.printStatement())
+    }
+
+    @Test
+    fun `when account holder deposits, balance is value after transaction`() {
+        val account = Account()
+        account.deposit(500)
+
+        val result = account.printStatement()
+
+        val row = result.split("\n")[1]
+        val balance = row.substring(22, 29)
+        assertEquals("    500", balance)
+    }
+
+//    @Test
+//    fun `when a deposit was previously done, the balance in the second deposit is the balance afterwards`() {
+//        val account = Account()
+//        account.deposit(500)
+//        account.deposit(300)
+//
+//        val result = account.printStatement()
+//
+//        val row = result.split("\n")[2]
+//        val balance = row.substring(22, 29)
+//        assertEquals("    800", balance)
+//    }
 }
